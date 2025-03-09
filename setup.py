c231f9ef771da1303465277282079cab1bb2ad56
paivalucass <lucaspaiva2805@gmail.com>
2025-03-09 12:52:43 -0300

    update setup info

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='IVN_Online_ML_IDS',
+    name='DQL_IVN_IDS',
     version='1.0',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
-    author='Lucas Paiva & Isabela Nascimento',
+    author='Lucas Paiva',
     # description='',
 )
