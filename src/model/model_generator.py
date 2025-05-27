58615272beec2088863f8b17c0a1ff5bcfd7a30c
paivalucass <lucaspaiva2805@gmail.com>
2025-05-27 10:17:50 -0300

    fix: dataset reading features

--- src/model/model_generator.py
+++ src/model/model_generator.py
@@ -81,12 +81,14 @@ class Environment():
             labels_array = pd.read_csv(paths_dictionary["y_path"], header=None, names=["index", "Class"])
             labels_array = labels_array.drop(columns=["index"])
             labels_array = labels_array.to_numpy()
+            
             features_array = features_array[self._start_train:self._end_train]
             labels_array = labels_array[self._start_train:self._end_train]
         
         else:
             labels_array = np.load(paths_dictionary["y_path"])
             labels_array = labels_array.f.arr_0
+            
             features_array = features_array[self._start_test:self._end_test]
             labels_array = labels_array[self._start_test:self._end_test]
         
