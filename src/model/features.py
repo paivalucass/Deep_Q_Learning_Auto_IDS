114ed00bd1a168d1b4b4aad7ad5131e037a81e61
paivalucass <lucaspaiva2805@gmail.com>
2025-07-10 20:18:56 -0300

    feat: add avtp dataset packets to framework

--- src/model/features.py
+++ src/model/features.py
@@ -30,7 +30,7 @@ class DQNFeatureGenerator():
         
     def generate_features(self):
         AVAILABLE_DATASETS = {
-            # "AVTP_Intrusion_dataset": self.__avtp_dataset_generate_features,
+            "AVTP_Intrusion_dataset": self.__avtp_dataset_generate_features,
             "TOW_IDS_dataset": self.__tow_ids_dataset_generate_features
         }
 
@@ -38,7 +38,6 @@ class DQNFeatureGenerator():
             raise KeyError(f"Selected dataset: {self._dataset} is NOT available for DQN IDS Feature Generator!")
 
         return AVAILABLE_DATASETS[self._dataset]()
-        
         
     def __tow_ids_dataset_generate_features(self):
         # Load raw packets
@@ -74,6 +73,64 @@ class DQNFeatureGenerator():
         print(f"labels multiclass: {labels_multiclass[:50]}")
         
         return preprocessed_packets, labels_binary, labels_multiclass
+    
+    def __avtp_dataset_generate_features(self):
+        # Load raw packets
+        raw_injected_only_packets = self.__read_raw_packets(self.paths_dictionary['injected_only_path'])
+        injected_only_packets_array = self.__convert_packages(raw_injected_only_packets)
+        
+        for injected_raw_packets_path in self.paths_dictionary['avtp_dataset_path']:
+            # Load raw packets
+            raw_packets = self.__read_raw_packets(injected_raw_packets_path)
+
+            # Convert loaded packets to np array with uint8_t size
+            packets_array = self.__convert_packages(raw_packets)
+
+            # Preprocess packets
+            preprocessed_packets = self.__preprocess_raw_packets(packets_array, split_into_nibbles=True)
+
+            # Generate labels
+            labels_binary = self.__generate_labels(packets_array, injected_only_packets_array)
+            
+        print(f"packets: {preprocessed_packets[:50]}")
+        print(f"size packets: {len(preprocessed_packets[0])}")
+        print(f"labels binary: {labels_binary[:50]}")
+        
+        return preprocessed_packets, labels_binary, None
+    
+    def __generate_labels(self, packets_list, injected_packets):
+        labels_list = []
+
+        for packet in packets_list:
+            current_label = 0
+
+            if self.__is_array_in_list_of_arrays(packet, injected_packets):
+                current_label = 1
+
+            labels_list.append(current_label)
+
+        return labels_list
+    
+    def __is_array_in_list_of_arrays(self, array_to_check, list_np_arrays):
+        # Reference:
+        # https://stackoverflow.com/questions/23979146/check-if-numpy-array-is-in-list-of-numpy-arrays
+        is_in_list = np.any(np.all(array_to_check == list_np_arrays, axis=1))
+
+        return is_in_list
+    
+    def __read_raw_packets(self, pcap_filepath):
+        raw_packets = rdpcap(pcap_filepath)
+
+        raw_packets_list = []
+
+        for packet in raw_packets:
+            if self._filter_avtp_packets:
+                if (len(packet) == AVTP_PACKETS_LENGHT):
+                    raw_packets_list.append(raw(packet))
+            else:
+                raw_packets_list.append(raw(packet))
+
+        return raw_packets_list
             
     def __convert_packages(self, raw_packets):
         
