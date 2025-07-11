0af7bd3a7aae017b3dce610f425ac7ef242ef6fa
paivalucass <lucaspaiva2805@gmail.com>
2025-07-11 17:09:33 -0300

    update shell script

--- quick_run.sh
+++ quick_run.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
 
-python /home/slurm/pesgradivn/lcap/Deep_Q_Learning_Auto_IDS/src/model/online_deep_q_network.py
+VENV_DIR="lacpvenv"
+REPOSITORY_PATH="Deep_Q_Learning_Auto_IDS"
+PYTHON_PATH="/home/slurm/pesgradivn/lcap/Deep_Q_Learning_Auto_IDS/src/features"
+PYTHON_FILE="main.py"
+JSON_PATH="/home/slurm/pesgradivn/lcap/Deep_Q_Learning_Auto_IDS/jsons/dql.json"
+
+if [ -d "$VENV_DIR" ]; then
+    echo "Activating virtual environment..."
+    source "$VENV_DIR/bin/activate"
+else
+    python3 -m venv $VENV_DIR
+    echo "$VENV_DIR virtual environment not found, creating one..."
+fi
+# Check if requirements.txt exists
+if [ -f "requirements.txt" ]; then
+    pip install -e .
+    echo "Installing packages from requirements.txt..."
+    pip install -r requirements.txt
+else
+    echo "requirements.txt not found. Make sure the repository folder is right"
+    exit 1
+fi
+
+cd ..
+
+python3 /home/slurm/pesgradivn/lcap/Deep_Q_Learning_Auto_IDS/src/model/online_deep_q_network.py
